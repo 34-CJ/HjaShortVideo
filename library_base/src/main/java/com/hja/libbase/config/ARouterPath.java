e24cfbb3271efef100707111a40b75d03efa82b2
hja <2530115347@qq.com>
2026-03-15 11:18:36 +0800

    feat: configure ARouter module names and base constant paths

---
+++ library_base/src/main/java/com/hja/libbase/config/ARouterPath.java
@@ -0,0 +1,43 @@
+package com.hja.libbase.config;
+
+public class ARouterPath {
+
+    /**
+     * 对应app主模块
+     */
+    public static class Main {
+
+        private static final String MAIN = "/main";
+
+        //对应MainActivity的路径
+        public static final String ACTIVITY_MAIN = MAIN + "/mainActivity";
+    }
+
+    public static class Home {
+
+        private static final String HOME = "/home";
+        public static final String FRAGMENT_HOME = HOME + "/homeFragment";
+    }
+
+    public static class Plaza {
+
+        private static final String PLAZA = "/plaza";
+        public static final String FRAGMENT_PLAZA = PLAZA + "/plazaFragment";
+    }
+
+    public static class Find {
+
+        private static final String FIND = "/find";
+        public static final String FRAGMENT_FIND = FIND + "/findFragment";
+    }
+
+    public static class User {
+
+        private static final String USER = "/user";
+        public static final String FRAGMENT_USER = USER + "/userFragment";
+
+        public static final String ACTIVITY_LOGIN = USER + "/loginActivity";
+    }
+
+
+}
