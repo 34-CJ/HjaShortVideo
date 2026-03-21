c64b7d9bd89921cd5c14147103803b14d9d60805
hja <2530115347@qq.com>
2026-03-21 13:16:30 +0800

    feat: define common network response models

---
+++ library_base/src/main/java/com/hja/libbase/bean/ResList.java
@@ -0,0 +1,25 @@
+package com.hja.libbase.bean;
+
+
+import java.util.List;
+
+public class ResList<T> {
+    private int count;
+    private List<T> list;
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public List<T> getList() {
+        return list;
+    }
+
+    public void setList(List<T> list) {
+        this.list = list;
+    }
+}
