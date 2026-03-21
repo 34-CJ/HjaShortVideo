6d4b6d81615860ca4642cd34f6f5a13f79e65737
hja <2530115347@qq.com>
2026-03-21 13:27:47 +0800

    feat: integrate everything into VideoListFragment

--- feature_home/src/main/java/com/hja/feature_home/fragment/videolist/VideoListFragment.java
+++ feature_home/src/main/java/com/hja/feature_home/fragment/videolist/VideoListFragment.java
@@ -1,30 +1,41 @@
 package com.hja.feature_home.fragment.videolist;
 
-import androidx.lifecycle.ViewModel;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.hja.feature_home.R;
-import com.hja.feature_home.adapter.ResVideo;
 import com.hja.feature_home.adapter.VideoAdapter;
+import com.hja.feature_home.bean.ResVideo;
 import com.hja.feature_home.config.HomeConfig;
 import com.hja.feature_home.databinding.LayoutFragmentVideoListBinding;
 import com.hja.libbase.base.BaseFragment;
 import com.hja.libbase.config.ARouterPath;
+import com.hja.libbase.config.ErrorStatusConfig;
+import com.hja.libbase.databinding.LayoutStatusViewBinding;
+import com.scwang.smart.refresh.layout.SmartRefreshLayout;
+import com.scwang.smart.refresh.layout.api.RefreshLayout;
+import com.scwang.smart.refresh.layout.listener.OnLoadMoreListener;
+import com.scwang.smart.refresh.layout.listener.OnRefreshListener;
 
-import java.util.ArrayList;
-
+import java.util.List;
 
 @Route(path = ARouterPath.Home.FRAGMENT_VIDEO_LIST)
 public class VideoListFragment extends BaseFragment<LayoutFragmentVideoListBinding, VideoListViewModel> {
 
+
     @Autowired(name = HomeConfig.KEY_VIDEO_LIST_TYPE)
     public int mPageType;
+    private VideoAdapter mAdapter;
+
     @Override
     protected VideoListViewModel getViewModel() {
-        return null;
+        return new ViewModelProvider(this).get(VideoListViewModel.class);
     }
 
     @Override
@@ -39,29 +50,95 @@ public class VideoListFragment extends BaseFragment<LayoutFragmentVideoListBindi
 
     @Override
     protected void initView() {
+        //初始化recyclerView
         LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
         mDataBinding.recyclerView.setLayoutManager(layoutManager);
+        //初始化适配器
+        mAdapter = new VideoAdapter();
+        mDataBinding.recyclerView.setAdapter(mAdapter);
+        //为smartRefreshLayout添加  加载更多的回调
+        mDataBinding.smartRefreshLayout.setOnLoadMoreListener(new OnLoadMoreListener() {
+            @Override
+            public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+                //下拉触发这个方法
+                mViewModel.requestData(mPageType, false);
+            }
+        });
+        //为smartRefreshLayout添加  刷新的回调
+        mDataBinding.smartRefreshLayout.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh(@NonNull RefreshLayout refreshLayout) {
+                //刷新
+                mViewModel.requestData(mPageType, true);
+            }
+        });
 
-        ArrayList<ResVideo> videos = new ArrayList<>();
-        videos.add(new ResVideo("标题1","作者9","00:23"));
-        videos.add(new ResVideo("标题2","作者8","13:23"));
-        videos.add(new ResVideo("标题3","作者7","00:43"));
-        videos.add(new ResVideo("标题4","作者6","00:23"));
-        videos.add(new ResVideo("标题5","作者5","00:63"));
-        videos.add(new ResVideo("标题6","作者3","30:83"));
-        videos.add(new ResVideo("标题7","作者3","00:23"));
-        videos.add(new ResVideo("标题8","作者2","80:23"));
-        videos.add(new ResVideo("标题9","作者0","00:23"));
-
-
-        mDataBinding.recyclerView.setAdapter(new VideoAdapter(videos));
-        if (mPageType == HomeConfig.VIDEO_LIST_FRAGMENT_RECOMMEND) {
-        } else if (mPageType == HomeConfig.VIDEO_LIST_FRAGMENT_DAILY) {
-        }
     }
 
     @Override
     protected void initData() {
+        //根据页面类型请求数据
+        mViewModel.requestData(mPageType, true);
+        //观测到数据发生变化
+        mViewModel.getDatas().observe(this, resVideoResList -> {
+            //停止刷新、加载的状态
+            SmartRefreshLayout smartRefreshLayout = mDataBinding.smartRefreshLayout;
+            if (smartRefreshLayout.isRefreshing()) {
+                smartRefreshLayout.finishRefresh();
+            }
+            if (smartRefreshLayout.isLoading()) {
+                smartRefreshLayout.finishLoadMore();
+            }
+            //将数据设置到适配器进行显示
+            List<ResVideo> resVideos = resVideoResList.getList();
+            mAdapter.setVideos(resVideos);
+        });
 
+        //是否允许加载更多
+        mViewModel.getIsLoadMore().observe(this, isLoadMore -> {
+            //是否允许继续加载
+            mDataBinding.smartRefreshLayout.setEnableLoadMore(isLoadMore);
+            if (!isLoadMore) {
+                Toast.makeText(getContext(), "没有更多数据了！", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        //错误状态显示
+        mViewModel.getErrorCode().observe(this, errorCode -> {
+            String content = "";
+            switch (errorCode) {
+                case ErrorStatusConfig.ERROR_STATUS_NETWORK_FIAL:
+                    content = "网络错误，请检查网络！";
+                    break;
+                case ErrorStatusConfig.ERROR_STATUS_NOT_LOGIN:
+                    content = "请登录后再进行操作！";
+                    break;
+                case ErrorStatusConfig.ERROR_STATUS_EMPTY:
+                    content = "当前没有更多数据了！";
+                    break;
+                case ErrorStatusConfig.ERROR_STATUS_SERVER_ERROR:
+                    content = "服务器异常！";
+                    break;
+                case ErrorStatusConfig.ERROR_STATUS_NORMAL:
+                default:
+                    content = "";
+                    break;
+            }
+
+            LayoutStatusViewBinding layoutStatusView = mDataBinding.layoutStatusView;
+            boolean isVisibility = errorCode != ErrorStatusConfig.ERROR_STATUS_NORMAL;
+            layoutStatusView.clStatusView.setVisibility(isVisibility ? View.VISIBLE : View.GONE);
+            layoutStatusView.tvLable.setText(content);
+
+        });
+    }
+
+    /**
+     * 测试用的方法， 用来测试错误码，并不是实际存在的功能
+     *
+     * @param value 自定义错误码
+     */
+    public void setErrorCode(Integer value) {
+        mViewModel.getErrorCode().setValue(value);
     }
 }
