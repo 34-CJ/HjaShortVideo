0e0102d371bb7e9e2eebd408b3c1a57f7f11045a
hja <2530115347@qq.com>
2026-03-21 13:24:04 +0800

    feat: create VideoListViewModel

--- feature_home/src/main/java/com/hja/feature_home/fragment/videolist/VideoListModel.java
+++ feature_home/src/main/java/com/hja/feature_home/fragment/videolist/VideoListModel.java
@@ -1,4 +1,89 @@
 package com.hja.feature_home.fragment.videolist;
 
+import android.util.Log;
+
+import com.hja.feature_home.api.HomeApiService;
+import com.hja.feature_home.bean.ResVideo;
+import com.hja.feature_home.config.HomeConfig;
+import com.hja.libbase.bean.ResBase;
+import com.hja.libbase.bean.ResList;
+import com.hja.libbase.config.ErrorStatusConfig;
+
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class VideoListModel {
+
+    private static final String TAG = "VideoListModel";
+
+    private final HomeApiService mApiService;
+    private IVideoListListenner mListenner;
+
+    private int mPage = 1;//当前请求的页数
+    private final int mLimit = 10;
+
+    public VideoListModel(IVideoListListenner listenner) {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://titok.fzqq.fun/")
+                .addConverterFactory(GsonConverterFactory.create())// 配置 Gson 转换器
+                .build();
+
+        mApiService = retrofit.create(HomeApiService.class);
+
+        mListenner = listenner;
+    }
+
+    /**
+     * 请求推荐页、日报页的数据
+     *
+     * @param pageType 是推荐页还是日报页？
+     * @param isFirst  是不是第一次加载？
+     */
+    public void requestData(int pageType, boolean isFirst) {
+        if (isFirst) {
+            mPage = 1;
+        } else {
+            mPage++;
+        }
+        Call<ResBase<ResList<ResVideo>>> call;
+        if (pageType == HomeConfig.VIDEO_LIST_FRAGMENT_RECOMMEND) {
+            call = mApiService.getRecommend(mPage, mLimit);
+        } else {
+            call = mApiService.getDaily(mPage, mLimit);
+        }
+        call.enqueue(new Callback<ResBase<ResList<ResVideo>>>() {
+            @Override
+            public void onResponse(Call<ResBase<ResList<ResVideo>>> call, Response<ResBase<ResList<ResVideo>>> response) {
+                //网络请求是否成功
+                if (response.isSuccessful()) {
+                    Log.i(TAG, "onResponse: 请求成功");
+                    ResBase<ResList<ResVideo>> body = response.body();
+                    if (body.getCode() == 1) {
+                        Log.i(TAG, "onResponse: 数据请求成功");
+                        if (body.getData().getList().size() > 0) {
+                            mListenner.onLoadFinish(isFirst, body.getData());
+                        } else {
+                            mListenner.onLoadFial(ErrorStatusConfig.ERROR_STATUS_EMPTY, "当前列表没有数据！");
+                        }
+                    } else {
+                        //服务器告诉我们，数据请求失败
+                        mListenner.onLoadFial(ErrorStatusConfig.ERROR_STATUS_SERVER_ERROR, body.getMsg());
+                    }
+                } else {
+                    mListenner.onLoadFial(ErrorStatusConfig.ERROR_STATUS_NETWORK_FIAL, "网络请求失败，请检查网络！");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResBase<ResList<ResVideo>>> call, Throwable throwable) {
+                //网络请求失败   超时、网络异常...
+                mListenner.onLoadFial(ErrorStatusConfig.ERROR_STATUS_NETWORK_FIAL, "网络请求失败，请检查网络！");
+            }
+        });
+    }
 }
--- feature_home/src/main/java/com/hja/feature_home/fragment/videolist/VideoListViewModel.java
+++ feature_home/src/main/java/com/hja/feature_home/fragment/videolist/VideoListViewModel.java
@@ -1,6 +1,87 @@
 package com.hja.feature_home.fragment.videolist;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class VideoListViewModel extends ViewModel {
+import com.hja.feature_home.bean.ResVideo;
+import com.hja.feature_home.fragment.videolist.IVideoListListenner;
+import com.hja.feature_home.fragment.videolist.VideoListModel;
+import com.hja.libbase.bean.ResList;
+import com.hja.libbase.config.ErrorStatusConfig;
+
+import java.util.List;
+
+public class VideoListViewModel extends ViewModel implements IVideoListListenner {
+
+    //model
+    private final VideoListModel mModel;
+
+    //列表需要使用的数据
+    public MutableLiveData<ResList<ResVideo>> mDatas = new MutableLiveData<>();
+    //错误码
+    public MutableLiveData<Integer> mErrorCode = new MutableLiveData<>();
+
+    //是否还能继续加载更多 默认是true
+    public MutableLiveData<Boolean> mIsLoadMore = new MutableLiveData<>(true);
+
+    public VideoListViewModel() {
+        mModel = new VideoListModel(this);
+    }
+
+    /**
+     * 根据页面类型请求数据
+     *
+     * @param pageType 页面类型：推荐、日报
+     * @param isFirst  是否是第一次加载
+     */
+    public void requestData(int pageType, boolean isFirst) {
+        if (isFirst) {
+            mIsLoadMore.setValue(true);
+        }
+        //发起网络请求
+        mModel.requestData(pageType, isFirst);
+    }
+
+    @Override
+    public void onLoadFinish(boolean isFirst, ResList<ResVideo> videos) {
+        if (isFirst) {
+            //第一次加载或者是刷新的情况
+            mDatas.setValue(videos);
+        } else {
+            //分页加载的情况
+            ResList<ResVideo> value = mDatas.getValue();
+            List<ResVideo> resVideos = value.getList();
+            resVideos.addAll(videos.getList());
+            mDatas.setValue(value);
+        }
+
+        //当前列表的总数
+        int count = videos.getCount();
+        if (mDatas.getValue().getList().size() >= count) {
+            mIsLoadMore.setValue(false);
+        }
+    }
+
+    @Override
+    public void onLoadFial(int statusCode, String message) {
+        mErrorCode.setValue(statusCode);
+        if (statusCode == ErrorStatusConfig.ERROR_STATUS_EMPTY) {
+            mIsLoadMore.setValue(false);
+        }
+    }
+
+
+    public MutableLiveData<ResList<ResVideo>> getDatas() {
+        return mDatas;
+    }
+
+    public MutableLiveData<Integer> getErrorCode() {
+        return mErrorCode;
+    }
+
+    public MutableLiveData<Boolean> getIsLoadMore() {
+        return mIsLoadMore;
+    }
+
+
 }
