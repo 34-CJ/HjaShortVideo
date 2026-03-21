753ef72f069751b242c23d05c9bcc00103cd5533
hja <2530115347@qq.com>
2026-03-21 13:18:59 +0800

    feat: implement HomeApiService and configure AndroidManifests for network access

---
+++ feature_home/src/main/java/com/hja/feature_home/api/HomeApiService.java
@@ -0,0 +1,20 @@
+package com.hja.feature_home.api;
+
+import com.hja.feature_home.bean.ResVideo;
+import com.hja.libbase.bean.ResBase;
+import com.hja.libbase.bean.ResList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface HomeApiService {
+
+
+    @GET("addons/cms/api.eye/daily")
+    Call<ResBase<ResList<ResVideo>>> getDaily(@Query("page") int page, @Query("limit") int limit);
+
+
+    @GET("addons/cms/api.eye/recommend")
+    Call<ResBase<ResList<ResVideo>>> getRecommend(@Query("page") int page, @Query("limit") int limit);
+}
