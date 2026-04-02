a05f44a5602650dde98beb6da83f7244feef84c8
hja <2530115347@qq.com>
2026-04-02 22:15:20 +0800

    feat: implement user center and fragment layout

--- feature_user/src/main/java/com/hja/feature_user/UserFragment.java
+++ feature_user/src/main/java/com/hja/feature_user/UserFragment.java
@@ -1,17 +1,19 @@
 package com.hja.feature_user;
 
-
-import androidx.lifecycle.ViewModel;
+import android.util.Log;
+import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.hja.feature_user.databinding.LayoutFragmentUserBinding;
 import com.hja.libbase.base.BaseFragment;
 import com.hja.libbase.base.BaseViewModel;
 import com.hja.libbase.config.ARouterPath;
+import com.hja.libbase.utils.StatusBarUtils;
 
 @Route(path = ARouterPath.User.FRAGMENT_USER)
-public class UserFragment extends BaseFragment {
-
+public class UserFragment extends BaseFragment<LayoutFragmentUserBinding, BaseViewModel> {
+    private static final String TAG = "UserFragment";
 
     @Override
     protected BaseViewModel getViewModel() {
@@ -30,7 +32,15 @@ public class UserFragment extends BaseFragment {
 
     @Override
     protected void initView() {
+        Log.i(TAG, "initView");
+        StatusBarUtils.addStatusBarHeight2Views(mDataBinding.getRoot(), mDataBinding.ivSettings, mDataBinding.ivQualifications);
 
+        mDataBinding.ivEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ARouter.getInstance().build(ARouterPath.User.ACTIVITY_LOGIN).navigation();
+            }
+        });
     }
 
     @Override
