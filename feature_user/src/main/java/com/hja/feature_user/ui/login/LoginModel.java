5c32e68700d1ea08922ce72d8cc3239b359b5fbd
hja <2530115347@qq.com>
2026-04-03 09:43:05 +0800

    feat: implement login and sms verification logic

--- feature_user/src/main/java/com/hja/feature_user/ui/login/LoginModel.java
+++ feature_user/src/main/java/com/hja/feature_user/ui/login/LoginModel.java
@@ -1,4 +1,63 @@
 package com.hja.feature_user.ui.login;
 
+import com.hja.feature_user.api.UserApiServiceProvider;
+import com.hja.feature_user.bean.ReqMobileLogin;
+import com.hja.feature_user.bean.ReqSendSmsCode;
+import com.hja.feature_user.bean.ResLogin;
+import com.hja.libbase.base.IRequestCallback;
+import com.hja.network.ApiCall;
+import com.hja.network.bean.ResBase;
+
+import retrofit2.Call;
+
 public class LoginModel {
+
+    /**
+     * 发送验证码
+     *
+     * @param mobile   手机号
+     * @param callback 回调
+     */
+    public void sendSmsCode(String mobile, IRequestCallback<ResBase<ResBase>> callback) {
+
+        ReqSendSmsCode smsCode = new ReqSendSmsCode(mobile, "mobilelogin");
+        Call<ResBase<ResBase>> call = UserApiServiceProvider.getApiService().sendSmsCode(smsCode);
+        ApiCall.enqueue(call, new ApiCall.ApiCallback<ResBase<ResBase>>() {
+            @Override
+            public void onSuccess(ResBase<ResBase> result) {
+                callback.onLoadFinish(result);
+            }
+
+            @Override
+            public void onError(int errorCode, String meesage) {
+                callback.onLoadFailure(errorCode, meesage);
+            }
+        });
+
+    }
+
+    /**
+     * 手机号登录
+     *
+     * @param mobile   手机号
+     * @param code     验证码
+     * @param callback 回调
+     */
+    public void mobileLogin(String mobile, String code, IRequestCallback<ResBase<ResLogin>> callback) {
+
+        ReqMobileLogin login = new ReqMobileLogin(mobile, code);
+        Call<ResBase<ResLogin>> call = UserApiServiceProvider.getApiService().mobileLogin(login);
+        ApiCall.enqueue(call, new ApiCall.ApiCallback<ResBase<ResLogin>>() {
+            @Override
+            public void onSuccess(ResBase<ResLogin> result) {
+                callback.onLoadFinish(result);
+            }
+
+            @Override
+            public void onError(int errorCode, String meesage) {
+                callback.onLoadFailure(errorCode, meesage);
+            }
+        });
+
+    }
 }
--- feature_user/src/main/java/com/hja/feature_user/ui/login/LoginViewModel.java
+++ feature_user/src/main/java/com/hja/feature_user/ui/login/LoginViewModel.java
@@ -1,22 +1,165 @@
 package com.hja.feature_user.ui.login;
 
+import android.os.CountDownTimer;
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class LoginViewModel extends ViewModel {
+import com.hja.feature_user.bean.ResLogin;
+import com.hja.libbase.base.BaseViewModel;
+import com.hja.libbase.base.IRequestCallback;
+import com.hja.network.bean.ResBase;
 
-    MutableLiveData<String> label = new MutableLiveData<>();
 
-    public void changeData(){
-        label.setValue("测试一下数据");
+public class LoginViewModel extends BaseViewModel {
+
+    private static final String TAG = "LoginViewModel";
+    private final LoginModel mModel;
+    private MutableLiveData<String> mUserMobile = new MutableLiveData<>();//用户输入的手机号
+    private MutableLiveData<String> mCode = new MutableLiveData<>();//用户输入的验证码
+    private MutableLiveData<Boolean> mIsEnableLogin = new MutableLiveData<>(false);//登录按钮是否可用。默认不可用
+    private MutableLiveData<Boolean> mCheckAgreement = new MutableLiveData<>(false);//登是否勾选协议
+
+    private MutableLiveData<String> mGetVerticalCodeText = new MutableLiveData<>("获取验证码");//获取验证码控件的显示文本
+    private MutableLiveData<Boolean> mIsEnableSendCode = new MutableLiveData<>(true);//获取验证码控件是否可用
+    private CountDownTimer mDownTimer;//获取验证码的倒计时
+
+
+    public LoginViewModel() {
+
+        mModel = new LoginModel();
     }
 
-    public LiveData<String> getLabel() {
-        return label;
+    /**
+     * 更新登录按钮的可用状态
+     */
+    public void updateEnableLoginBtnStatus() {
+
+        String mobile = mUserMobile.getValue();
+        String code = mCode.getValue();
+
+        //排除一些不需要更新状态的情况
+        if (mobile == null || code == null) {
+            return;
+        }
+        boolean isEnable = mobile.length() == 11 && code.length() == 4;
+        mIsEnableLogin.setValue(isEnable);
+
     }
 
-    public void setLabel(MutableLiveData<String> label) {
-        this.label = label;
+    /**
+     * 发送验证码
+     */
+    public void sendCode() {
+
+        String mobile = mUserMobile.getValue();
+        if (mobile == null || mobile.length() != 11) {
+            Log.i(TAG, "sendCode: 手机号不符合规则！");
+            showToast("请输入正确的手机号码！");
+            return;
+        }
+
+
+        if (mDownTimer != null) {
+            mDownTimer.cancel();//防止重复点击时 未停止之前的计时
+        }
+
+        //禁用发送按钮
+        mIsEnableSendCode.setValue(false);
+
+        mDownTimer = new CountDownTimer(60000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                //每1秒会被触发 把毫秒转为秒
+                int seconds = (int) (millisUntilFinished / 1000);
+                mGetVerticalCodeText.setValue(seconds + "s");//更新倒计时的显示
+            }
+
+            @Override
+            public void onFinish() {
+                //倒计时完成后
+                mGetVerticalCodeText.setValue("获取验证码");
+                //60s后允许发送验证码
+                mIsEnableSendCode.setValue(true);
+            }
+        }.start();
+
+        //发起请求，让服务端发送验证码
+        Log.i(TAG, "sendCode: ");
+        showLoading(true);
+        //发起获取验证码请求
+        mModel.sendSmsCode(mobile, new IRequestCallback<ResBase<ResBase>>() {
+            @Override
+            public void onLoadFinish(ResBase<ResBase> datas) {
+                showToast(datas.getMsg());//显示消息弹窗
+                showLoading(false);
+            }
+
+            @Override
+            public void onLoadFailure(int errorCode, String message) {
+                showToast(message);//显示消息弹窗
+                showLoading(false);
+            }
+        });
     }
+
+    /**
+     * 登录
+     */
+    public void login() {
+        Boolean checkAgreement = mCheckAgreement.getValue();
+        if (!checkAgreement) {
+            showToast("请先同意用户协议与隐私政策");
+            Log.i(TAG, "请先同意用户协议与隐私政策");
+            return;
+        }
+        showLoading(true);
+
+        String mobile = mUserMobile.getValue();
+        String code = mCode.getValue();
+
+        mModel.mobileLogin(mobile, code, new IRequestCallback<ResBase<ResLogin>>() {
+            @Override
+            public void onLoadFinish(ResBase<ResLogin> datas) {
+                Log.i(TAG, "onLoadFinish token：" + datas.getData());
+                showLoading(false);
+                showToast(datas.getMsg());
+            }
+
+            @Override
+            public void onLoadFailure(int errorCode, String message) {
+                showToast(message);
+                showLoading(false);
+            }
+        });
+    }
+
+    public MutableLiveData<Boolean> getIsEnableLogin() {
+        return mIsEnableLogin;
+    }
+
+    public MutableLiveData<String> getUserMobile() {
+        return mUserMobile;
+    }
+
+    public MutableLiveData<String> getCode() {
+        return mCode;
+    }
+
+    public MutableLiveData<Boolean> getCheckAgreement() {
+        return mCheckAgreement;
+    }
+
+    public MutableLiveData<String> getGetVerticalCodeText() {
+        return mGetVerticalCodeText;
+    }
+
+    public MutableLiveData<Boolean> getIsEnableSendCode() {
+        return mIsEnableSendCode;
+    }
+
+
 }
