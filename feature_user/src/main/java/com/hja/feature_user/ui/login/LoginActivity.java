a4c423a18c8d68253c44740952b6bad00ff85e57
hja <2530115347@qq.com>
2026-04-05 09:49:05 +0800

    feat: implement login activity

--- feature_user/src/main/java/com/hja/feature_user/ui/login/LoginActivity.java
+++ feature_user/src/main/java/com/hja/feature_user/ui/login/LoginActivity.java
@@ -1,21 +1,25 @@
 package com.hja.feature_user.ui.login;
 
-import android.os.Bundle;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.view.View;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.annotation.NonNull;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.alibaba.android.arouter.launcher.ARouter;
 import com.hja.feature_user.BR;
-
 import com.hja.feature_user.R;
 import com.hja.feature_user.databinding.ActivityLoginBinding;
 import com.hja.libbase.base.BaseActivity;
 import com.hja.libbase.config.ARouterPath;
+import com.hja.libbase.utils.StatusBarUtils;
 
 @Route(path = ARouterPath.User.ACTIVITY_LOGIN)
 public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewModel> {
@@ -24,12 +28,75 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
     protected void initView() {
         //在这里做一些布局、ui的初始化
 //        mDataBinding.main.setVisibility(View.INVISIBLE);
+
+        StatusBarUtils.addStatusBarHeight2Views(mDataBinding.getRoot(), mDataBinding.ivBack, mDataBinding.ivSettings);
+
+        mViewModel.getUserMobile().observe(this, mobile -> {
+            mViewModel.updateEnableLoginBtnStatus();
+        });
+        mViewModel.getCode().observe(this, code -> {
+            mViewModel.updateEnableLoginBtnStatus();
+        });
+
+        initAgreementText();
+
+
+        mDataBinding.ivBack.setOnClickListener(v -> {
+            finish();
+        });
+        mDataBinding.ivQualifications.setOnClickListener(v -> {
+            ARouter.getInstance().build(ARouterPath.User.ACTIVITY_AGREEMENT).navigation();
+        });
+    }
+
+    private void initAgreementText() {
+        String string = "请阅读并同意《用户协议》和《隐私政策》";
+        //借助SpannableString包装处理字符串内容
+        SpannableString spannableString = new SpannableString(string);
+        ClickableSpan clickableSpan1 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                ARouter.getInstance().build(ARouterPath.User.ACTIVITY_AGREEMENT).navigation();
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+
+                ds.setColor(Color.BLACK);
+//                ds.setUnderlineText(false);
+                ds.setTypeface(Typeface.DEFAULT_BOLD);
+
+            }
+        };
+
+        ClickableSpan clickableSpan2 = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View widget) {
+                ARouter.getInstance().build(ARouterPath.User.ACTIVITY_AGREEMENT).navigation();
+            }
+
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+
+                ds.setColor(Color.BLACK);
+//                ds.setUnderlineText(false);
+                ds.setTypeface(Typeface.DEFAULT_BOLD);
+
+            }
+        };
+
+        spannableString.setSpan(clickableSpan1, 6, 12, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(clickableSpan2, 14, 19, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        mDataBinding.cbAgreen.setText(spannableString);
+        mDataBinding.cbAgreen.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
     protected void initData() {
-        //在这里做一些数据上的初始化
-        mViewModel.changeData();
+
+
     }
 
     @Override
@@ -46,6 +113,5 @@ public class LoginActivity extends BaseActivity<ActivityLoginBinding, LoginViewM
     protected int getBindingVariableId() {
         return BR.viewModel;
     }
-
 
 }
