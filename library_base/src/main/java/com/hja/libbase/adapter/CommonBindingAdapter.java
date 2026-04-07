fe0f8403b566128e89cfc4bc5a5902b94d94ae9d
hja <2530115347@qq.com>
2026-04-07 13:22:22 +0800

    feat: Create base classes and ViewBinding tools

--- library_base/src/main/java/com/hja/libbase/adapter/CommonBindingAdapter.java
+++ library_base/src/main/java/com/hja/libbase/adapter/CommonBindingAdapter.java
@@ -7,6 +7,7 @@ import androidx.databinding.BindingAdapter;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
+import com.hja.libbase.R;
 
 public class CommonBindingAdapter {
 
@@ -15,14 +16,18 @@ public class CommonBindingAdapter {
         if (url != null && !url.isEmpty()) {
             Glide.with(imageView.getContext())
                     .load(url)
+                    .error(R.mipmap.bg_default)
                     .into(imageView);
         }
     }
+
     @BindingAdapter("imageCircleUrl")
     public static void loadCircleImage(ImageView imageView, String url) {
         if (url != null && !url.isEmpty()) {
             Glide.with(imageView.getContext())
                     .load(url)
+                    .placeholder(R.mipmap.icon_default_avatar)//加载过程的占位图
+                    .error(R.mipmap.icon_default_avatar)//加载失败时候的占位图
                     .apply(RequestOptions.bitmapTransform(new CircleCrop()))//加载圆形图片
                     .into(imageView);
         }
--- library_base/src/main/java/com/hja/libbase/base/BaseApplication.java
+++ library_base/src/main/java/com/hja/libbase/base/BaseApplication.java
@@ -1,6 +1,7 @@
 package com.hja.libbase.base;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.alibaba.android.arouter.BuildConfig;
 import com.alibaba.android.arouter.launcher.ARouter;
@@ -12,9 +13,13 @@ import me.jessyan.autosize.AutoSizeConfig;
  */
 public class BaseApplication extends Application {
 
+    private static Application instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        instance = this;
 
         //在调试模式下
         if (BuildConfig.DEBUG) {
@@ -24,7 +29,20 @@ public class BaseApplication extends Application {
 
         //初始化ARouter
         ARouter.init(this);
+
         //AndroidAutoSize的参数初始化
         AutoSizeConfig.getInstance().setCustomFragment(true);
+    }
+
+    /**
+     * 使用Application生成了一个全局可用的context
+     * 注意不要滥用，否则会产生下面的问题
+     * 1、把Application当成是某个Activity上下文，与ui更新关联在一起，会引发错误
+     * 2、更容易获取到context，会增加项目耦合性
+     *
+     * @return
+     */
+    public static Context getContext() {
+        return instance.getApplicationContext();
     }
 }
