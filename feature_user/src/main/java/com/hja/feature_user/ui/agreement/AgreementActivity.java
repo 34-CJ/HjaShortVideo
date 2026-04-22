2028d3a9ab73e20671a65a30413933eb33958362
hja <2530115347@qq.com>
2026-04-22 13:24:16 +0800

    feat: add user agreements and permission declaration pages

--- feature_user/src/main/java/com/hja/feature_user/ui/agreement/AgreementActivity.java
+++ feature_user/src/main/java/com/hja/feature_user/ui/agreement/AgreementActivity.java
@@ -5,8 +5,11 @@ import android.webkit.WebViewClient;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import com.alibaba.android.arouter.facade.annotation.Autowired;
 import com.alibaba.android.arouter.facade.annotation.Route;
+import com.hja.feature_user.BR;
 import com.hja.feature_user.R;
+import com.hja.feature_user.config.UserConfig;
 import com.hja.feature_user.databinding.ActivityAgreementBinding;
 import com.hja.libbase.base.BaseActivity;
 import com.hja.libbase.base.BaseViewModel;
@@ -16,8 +19,13 @@ import com.hja.libbase.utils.StatusBarUtils;
 @Route(path = ARouterPath.User.ACTIVITY_AGREEMENT)
 public class AgreementActivity extends BaseActivity<ActivityAgreementBinding, BaseViewModel> {
 
-    private final String URL = "https://titok.fzqq.fun/agreement.html";
+    private final String BASE_URL = "https://titok.fzqq.fun/";
+    private final String PRIVATE_URL = BASE_URL + "agreement.html";//隐私政策、隐私概要
+    private final String AGREEMENT_URL = BASE_URL + "UserAgreement.html";//用户协议
+    private final String USER_INFO_URL = BASE_URL + "userinfomenus.html";//隐私政策、隐私概要
 
+    @Autowired(name = UserConfig.AgreementType.KEY_AGREEMENT_TYPE)
+    public int mType;
 
     @Override
     protected BaseViewModel getViewModel() {
@@ -31,12 +39,13 @@ public class AgreementActivity extends BaseActivity<ActivityAgreementBinding, Ba
 
     @Override
     protected int getBindingVariableId() {
-        return 0;
+        return BR.viewModel;
     }
 
     @Override
     protected void initView() {
         StatusBarUtils.addStatusBarHeight2RootView(mDataBinding.getRoot());
+
     }
 
     @Override
@@ -52,7 +61,25 @@ public class AgreementActivity extends BaseActivity<ActivityAgreementBinding, Ba
                 mViewModel.showLoading(false);
             }
         });
-        mDataBinding.webView.loadUrl(URL);
+
+        String url = AGREEMENT_URL;
+        switch (mType) {
+            case UserConfig.AgreementType.VALUE_AGREEMENT:
+                url = AGREEMENT_URL;
+                mDataBinding.tvTitle.setText("用户协议");
+                break;
+            case UserConfig.AgreementType.VALUE_SIMPLE_PRIVATE:
+            case UserConfig.AgreementType.VALUE_PRIVATE:
+                url = PRIVATE_URL;
+                mDataBinding.tvTitle.setText("隐私政策");
+                break;
+            case UserConfig.AgreementType.VALUE_USER_INFO:
+                url = USER_INFO_URL;
+                mDataBinding.tvTitle.setText("个人信息收集清单");
+                break;
+        }
+
+        mDataBinding.webView.loadUrl(url);
 
     }
 }
---
+++ feature_user/src/main/java/com/hja/feature_user/ui/permission/PermissionActivity.java
@@ -0,0 +1,29 @@
+package com.hja.feature_user.ui.permission;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.alibaba.android.arouter.facade.annotation.Route;
+import com.hja.feature_user.R;
+import com.hja.libbase.config.ARouterPath;
+
+@Route(path = ARouterPath.User.ACTIVITY_PERMISSION)
+public class PermissionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_permission);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
